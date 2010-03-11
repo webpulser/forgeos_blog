a8cd78f3faf5d0b3d729d4948de47cccdb2303aa
cyrill62 <cyril.lepagnot@webpulser.com>
2010-03-11 17:00:43 +0100

    fix blog route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 ActionController::Routing::Routes.draw do |map|
   map.namespace :blog do |blog|
     blog.resources :posts, :has_many => :comments, :only => [:show,:index]
-    blog.root :controller => 'posts', :action => [:index]
+    blog.root :controller => 'posts', :action => 'index'
   end
 
   map.namespace :admin do |admin|
