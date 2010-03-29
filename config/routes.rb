f1506c91524856ba8bc5d27b78b701270f959053
Cyril Lepagnot <cyril.lepagnot@webpulser.com>
2010-03-29 11:21:11 +0200

    fix post_categories tree

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 ActionController::Routing::Routes.draw do |map|
   map.namespace :blog do |blog|
     blog.resources :posts, :has_many => :comments, :only => [:show,:index]
+    blog.post_category 'categories/:blog_category_id', :controller => 'posts', :action => 'index'
     blog.root :controller => 'posts', :action => 'index'
   end
 
   map.namespace :admin do |admin|
     admin.resources :posts, :has_many => :comments
+    admin.resources "post_categories", :controller => 'categories', :requirements => { :type => "post_category" }
   end
 end
