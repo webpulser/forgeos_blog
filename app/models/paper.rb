1a68a29f06ce5d028e19f9de76d8f047b007480d
Sébastien Dumont <sebastien.dumont@webpulser.com>
2010-07-26 11:44:31 +0200

    paper: fix aasm transitions

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -8,7 +8,7 @@ class Paper < ActiveRecord::Base
   belongs_to :author, :foreign_key => 'person_id', :class_name => 'Person'
   has_one :meta_info, :as => :target
   accepts_nested_attributes_for :meta_info
-  
+
   has_and_belongs_to_many :categories, :readonly => true, :join_table => 'categories_elements', :foreign_key => 'element_id', :association_foreign_key => 'category_id', :class_name => 'PaperCategory'
 
   translates :name, :content, :url, :description
@@ -21,11 +21,11 @@ class Paper < ActiveRecord::Base
   aasm_state :published
 
   aasm_event :publish do
-    transitions :to => :draft, :from => :published
+    transitions :to => :published, :from => :draft
   end
 
   aasm_event :throw do
-    transitions :to => :published, :from => :draft
+    transitions :to => :draft, :from => :published
   end
 
   def aasm_current_state_with_event_firing=(state)
@@ -34,6 +34,6 @@ class Paper < ActiveRecord::Base
       aasm_fire_event(event_name,false) if event && event.all_transitions.any?{ |t| t.to == state || t.to == state.to_sym }
     end
   end
-  
+
   alias_method :aasm_current_state_with_event_firing, :aasm_current_state
 end
