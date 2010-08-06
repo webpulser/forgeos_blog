3bcdcf94a98a8ef42e0e43132162ab790dc91f9b
Sébastien Dumont <sebastien.dumont@webpulser.com>
2010-08-06 11:09:24 +0200

    papers: improve latest posts

--- app/controllers/blog/tags_controller.rb
+++ app/controllers/blog/tags_controller.rb
@@ -1,20 +1,20 @@
 class Blog::TagsController < ApplicationController
   before_filter :get_defaults
-  
+
   def index
     if params[:tag_name]
-     
+
       paginate_options = {
         :page => params[:page],
         :per_page => (params[:per_page] || 5),
         :conditions => { :state => 'published'}
       }
-      
+
       tags = ActsAsTaggableOn::Tagging.all(
         :include => :tag,
-        :conditions => { 
-          :tags => { 
-            :name => params[:tag_name].humanize 
+        :conditions => {
+          :tags => {
+            :name => params[:tag_name].humanize
           }
         }
       )
@@ -24,11 +24,11 @@ class Blog::TagsController < ApplicationController
       end
     end
   end
-    
-    
+
+
 private
   def get_defaults
-    @last_papers = Paper.all(:order => 'created_at DESC', :limit => 5)
+    @last_papers = Paper.latest(:limit => 5)
     @categories = PaperCategory.all
   end
 end
