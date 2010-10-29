6aa453f34d73cd6091b6979944e8c5fab188b435
Cyril Lepagnot <cyril.lepagnot@webpulser.com>
2010-10-29 10:02:17 +0200

    add seo urls for paper categories

--- app/controllers/blog/papers_controller.rb
+++ app/controllers/blog/papers_controller.rb
@@ -1,16 +1,15 @@
 class Blog::PapersController < ApplicationController
   before_filter :get_defaults
   caches_page :show, :if => Proc.new { |c| c.request.format != 'js' }
-  
+
   def index
-    
     if params[:tag_name]
       @tags = ActsAsTaggableOn::Tagging.all(:include => :tag, :conditions => { :tags => { :name => params[:tag_name].humanize }})
     else
       @category = PaperCategory.find_by_url(params[:blog_category_id]) || PaperCategory.find_by_id(params[:blog_category_id]) if params[:blog_category_id]
     end
-    
-    paginate_options = {:page => params[:page], :per_page => (params[:per_page] || 5), :conditions => { :state => 'published'}}
+
+    paginate_options = {:page => page, :per_page => per_page, :conditions => { :state => 'published'}}
     case params[:sort_by]
     when 'popularity'
       paginate_options[:order] = "sum(#{PaperViewedCounter.table_name}.counter) DESC, papers.id DESC"
@@ -23,13 +22,13 @@ class Blog::PapersController < ApplicationController
     else
       paginate_options[:order] = 'papers.published_at DESC, papers.id DESC'
     end
-    
+
     if params[:tag_name]
       @paper = @tags.collect{ |t| t.papers }.paginate(paginate_options)
     else
       @papers = @category ? @category.elements.paginate(paginate_options) : Paper.paginate(paginate_options)
     end
-    
+
     respond_to do |format|
       format.html
       format.atom
@@ -56,5 +55,13 @@ class Blog::PapersController < ApplicationController
     @last_comments = Comment.latest_paper_comments(:limit => 5)
     @last_papers = Paper.latest(:limit => 5)
     @categories = PaperCategory.all
+  end
+
+  def page
+    params[:page].to_i > 0 ? params[:page] : 1
+  end
+
+  def per_page
+    params[:per_page].to_i > 0 ? params[:per_page] : 5
   end
 end
--- app/helpers/blog_helper.rb
+++ app/helpers/blog_helper.rb
@@ -2,28 +2,30 @@ module BlogHelper
   def seo_paper_path(paper, options={})
     blog_paper_path(options.merge(:id => paper.url))
   end
-  
+
   def seo_paper_cache_sweeper_path(url, options={})
     blog_paper_path(options.merge(:id => url))
   end
-  
+
+  def seo_blog_paper_category_path(category, options={})
+    blog_paper_category_path(options.merge(:blog_category_id => category.url))
+  end
+
+
   def tags
     # FIXME count taggable only if it state is 'published'
     ActsAsTaggableOn::Tag.count(:name, :joins => 'INNER JOIN taggings ON taggings.tag_id = tags.id' , :conditions => { :taggings => { :taggable_type => 'Paper'}}, :group => 'name', :select => 'name', :limit => 100 )
   end
-  
+
   def tag_cloud(tags, classes)
-    max, min = 0, 0
-    tags.each { |t|
-      max = t[1].to_i if t[1].to_i > max
-      min = t[1].to_i if t[1].to_i < min
-    }
-  
+    counts = tags.map(&:last)
+    min = counts.min
+    max = counts.max
+
     divisor = ((max - min) / classes.size) + 1
-  
-    tags.each { |t|
+
+    tags.map do |t|
       yield t[0], classes[(t[1].to_i - min) / divisor]
-    }
+    end
   end
-  
 end
