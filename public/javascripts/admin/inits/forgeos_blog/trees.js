c5d7dbdb6775e445200f12db05781e3a5823b4f7
Cyril Lepagnot <cyril.lepagnot@webpulser.com>
2011-04-12 16:11:34 +0200

    use new association_categories theme for trees

--- public/javascripts/admin/inits/forgeos_blog/trees.js
+++ public/javascripts/admin/inits/forgeos_blog/trees.js
@@ -3,5 +3,5 @@ jQuery(document).ready(function(){
   init_category_tree("#paper-tree",'PaperCategory','/admin/paper_categories.json');
 
   // init the tress for category associations
-  init_association_category_tree('#association-paper-tree', 'paper');
+  init_association_category_tree('#association-paper-tree', 'paper', 'category', 'association_categories');
 });
